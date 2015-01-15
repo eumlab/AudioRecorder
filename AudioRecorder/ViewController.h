c3bcb75025cf446931f2fd41f4c3a436e393b266
Nicholas Tau <fengping.tao@upai.com>
2015-01-15 19:29:57 +0800

    init project.

---
+++ AudioRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioRecorder
+//
+//  Created by Nicholas Tau on 1/15/15.
+//  Copyright (c) 2015 Nicholas Tau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
