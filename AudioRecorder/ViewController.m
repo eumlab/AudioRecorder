c9a6ff883285643910d8a9ac0a422df4eb0adb90
Nicholas Tau <objc.me@gmail.com>
2015-01-29 11:28:54 +0800

    init works here.

--- AudioRecorder/ViewController.m
+++ AudioRecorder/ViewController.m
@@ -14,11 +14,50 @@
 #import <AEBlockAudioReceiver.h>
 #import "NSString+DocumentDirectory.h"
 
-@interface ViewController ()
+typedef enum : NSUInteger {
+    kRecordFinsied,
+    kRecordPrerecording,
+    kRecordRecording,
+} kRecordState;
 
+static NSString * const kPreRecordFileName  = @"PreRecord.aiff";
+static NSString * const kRealRecordFileName = @"RealRecord.aiff";
+static NSString * const kConnectedFileName  = @"ConnectedRecord.aiff";
+
+@interface ViewController ()
+@property (nonatomic,assign) kRecordState recordState;
+@property (nonatomic,strong) NSMutableArray * sampleCacheList;
+
+@property (nonatomic,strong) AEAudioController * audioController;
+@property (nonatomic,strong) AEBlockAudioReceiver * audioReceiver;
+
+@property (nonatomic,assign) UInt32 maxMemoryCacheCount;
+
+@property (nonatomic,strong) AEAudioFileWriter * fileWriter;
 @end
 
 @implementation ViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        AudioStreamBasicDescription basicDescription =
+        [AEAudioController nonInterleaved16BitStereoAudioDescription];
+        self.audioController =
+        [[AEAudioController alloc] initWithAudioDescription:basicDescription
+                                               inputEnabled:YES];
+        [[NTAudioDataHelper sharedInstance] initWithNumChannels:basicDescription.mChannelsPerFrame
+                                                     sampleRate:basicDescription.mSampleRate];
+        _audioController.preferredBufferDuration = 0.005;
+        _audioController.useMeasurementMode = YES;
+        self.maxMemoryCacheCount = 15/_audioController.preferredBufferDuration;
+        [_audioController start:NULL];
+        
+        self.fileWriter = [[AEAudioFileWriter alloc] initWithAudioDescription:basicDescription];
+    }
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
